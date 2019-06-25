f9ce9ad0baaadf4a5f0184b4d2da22384be49b4c
Rafael Rivera <rafael@withinwindows.com>
2019-06-25 00:51:11 -0700

    Pipeline CI for dev head

--- EarTrumpet/prebuild.ps1
+++ EarTrumpet/prebuild.ps1
@@ -1,27 +1,7 @@
-﻿cd $PSScriptRoot
+﻿param($version)
 
-function UpdateManifest { param($path, $version)
-	$xml = [xml](Get-Content $path)
-	$xml.Package.Identity.Version = "$version"
-	$xml.Save((Resolve-Path ("$PSScriptRoot\$path")))
-}
+$PackageManifestPath = "$PSScriptRoot\..\EarTrumpet.Package\Package.appxmanifest"
 
-$PackageManifestPath = "..\EarTrumpet.Package\Package.appxmanifest"
-$AssemblyInfoPath = ".\Properties\AssemblyInfo.cs"
-
-$newVersion = Get-Content -raw ..\version.txt
-
-UpdateManifest -path $PackageManifestPath -version $newVersion
-
-$assemblyInfo = Get-Content -raw $AssemblyInfoPath
-
-if ($assemblyInfo -match ".*?Version\(`"(.*?)`"\).*?") {
-	$assemblyInfo = $assemblyInfo.Replace($Matches[1], $newVersion);
-	Set-Content -Path $AssemblyInfoPath -Value $assemblyInfo.Trim();
-} else {
-	throw "Unexpected lack of AssemblyInfo.cs match when searching for version";
-}
-
-Set-Content .\Assets\DevVersion.txt $newVersion
-
-write-host "Version: $newVersion from version.txt"
+$xml = [xml](Get-Content $PackageManifestPath)
+$xml.Package.Identity.Version = "$version"
+$xml.Save($PackageManifestPath)
