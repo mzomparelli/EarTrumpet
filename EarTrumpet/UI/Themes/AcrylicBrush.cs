aeefc7dd4c160453c98e0a378051e74daab8cbb2
Dave Amenta <dave@daveamenta.com>
2019-06-28 15:40:05 -0700

    Move Acrylic into a brush

---
+++ EarTrumpet/UI/Themes/AcrylicBrush.cs
@@ -0,0 +1,39 @@
+﻿using EarTrumpet.Interop.Helpers;
+using System;
+using System.Windows;
+
+namespace EarTrumpet.UI.Themes
+{
+    class AcrylicBrush
+    {
+        public static string GetBackground(DependencyObject obj) => (string)obj.GetValue(BackgroundProperty);
+        public static void SetBackground(DependencyObject obj, string value) => obj.SetValue(BackgroundProperty, value);
+        public static readonly DependencyProperty BackgroundProperty =
+        DependencyProperty.RegisterAttached("Background", typeof(string), typeof(AcrylicBrush), new PropertyMetadata("", BackgroundChanged));
+        private static void BackgroundChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var window = (Window)dependencyObject;
+            window.SourceInitialized += (_, __) =>
+            {
+                ApplyAcrylicToWindow(window, (string)e.NewValue);
+            };
+
+            var strongWindow = new WeakReference(window);
+            Manager.Current.ThemeChanged += () =>
+            {
+                var resolvedWindow = (Window)strongWindow.Target;
+                if (resolvedWindow != null)
+                {
+                    ApplyAcrylicToWindow(resolvedWindow, (string)e.NewValue);
+                }
+            };
+        }
+
+        private static void ApplyAcrylicToWindow(Window window, string refValue)
+        {
+            AccentPolicyLibrary.EnableAcrylic(window,
+                Manager.Current.ResolveRef(window, refValue),
+                Interop.User32.AccentFlags.DrawAllBorders);
+        }
+    }
+}
